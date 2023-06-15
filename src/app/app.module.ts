6e2d30b23d3add2d9e11662af43ae0db5c6f4f19
Bit <hannitah28@gmail.com>
2023-06-14 21:45:45 -0700

    multiple pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
 import { ProductsPageComponent } from './products-page/products-page.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     CarruselComponent,
-    ProductsPageComponent
+    ProductsPageComponent,
+    ProductListComponent,
+    StartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: StartPageComponent },
+      { path: 'products/',component:ProductsPageComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/start-page/start-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+@Component({
+  selector: 'app-start-page',
+  templateUrl: './start-page.component.html',
+  styleUrls: ['./start-page.component.css']
+})
+
+export class StartPageComponent {
+
+}
