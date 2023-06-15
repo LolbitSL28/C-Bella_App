bb94763c0728cc50959dac57b2942856956d4caf
Bit <hannitah28@gmail.com>
2023-06-14 20:47:18 -0700

    product page generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CarruselComponent } from './carrusel/carrusel.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    CarruselComponent
+    CarruselComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule
